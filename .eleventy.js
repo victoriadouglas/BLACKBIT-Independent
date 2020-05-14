23fdb1cfa82a078044e705ac4197bb1aa23f3669
Victoria Sellers <victoria.blackbit@gmail.com>
2020-05-14 13:59:51 -0400

    navbar responsive

--- .eleventy.js
+++ .eleventy.js
@@ -41,6 +41,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/_includes/img": "img" });
   eleventyConfig.addPassthroughCopy({ "src/_includes/css": "css" });
   eleventyConfig.addPassthroughCopy({ "src/_includes/js": "js" });
+  eleventyConfig.addPassthroughCopy({ "src/_includes/fonts": "webfonts" });
 
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
