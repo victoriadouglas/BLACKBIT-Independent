2d68d895ed3531fcef19c94c48e9b629503e5055
Victoria Sellers <victoria.blackbit@gmail.com>
2025-05-29 13:13:38 -0400

    Inifinite Marquee

--- src/_includes/js/gsap.js
+++ src/_includes/js/gsap.js
@@ -8,11 +8,46 @@ document.addEventListener("DOMContentLoaded", (event) => {
         smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
       });
 
+      gsap.set("#heading", { opacity: 1 });
 
-    
-   });
+let split = SplitText.create("#heading", { type: "chars" });
+//now animate each character into place from 20px below, fading in:
+gsap.from(split.chars, {
+  y: 20,
+  autoAlpha: 0,
+  stagger: 0.05
+});
+
+});
 
 
 
 
-// create the scrollSmoother before your scrollTriggers
+// Marquee Banner Scrolling
+const init = () => {
+    const marquee = document.getElementById('marquee-container')
+    if (!marquee){
+        return;      
+    }
+    const marqueeContent = document.getElementById('marquee-content');
+    if (!marqueeContent){
+        return;      
+    }
+
+    const width = parseInt( window.getComputedStyle(marqueeContent).getPropertyValue("width"),10
+    );
+
+    gsap.fromTo(
+        ".marquee",
+        { x:0 },
+        { 
+            x:-1*width,
+            duration: 20,
+            ease: "none",
+            repeat: -1
+        }
+    )
+
+}
+
+document.addEventListener('DOMContentLoaded',init);
